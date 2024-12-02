41a1b53b9a9e124eb5f95fbb4422f86b85f11668
christoph-sachse-sonarsource <christoph.sachse@sonarsource.com>
2024-12-02 13:27:43 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ gsasg
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener noreferrer yayayay"
         >
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
